876f0ca0b09a186f70387c16b0aa8d33c32fe2c8
anna <annajuliamarcelino@gmail.com>
2026-09-14 16:40:04 -0300

    ssaadd

--- DoceCantinho.Infrastructure/Migrations/20260914174612_doce1.Designer.cs
+++ DoceCantinho.Infrastructure/Migrations/20260914174612_doce1.Designer.cs
@@ -12,7 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoceCantinho.Infrastructure.Migrations
 {
     [DbContext(typeof(DoceCantinhoDbContext))]
-    [Migration("20260914174612_doce1")]
+    [Migration("20260914165322_doce1")]
+
     partial class doce1
     {
         /// <inheritdoc />
