31c78d021abe502b3cb18717e645af49aa087265
Guilherme Martins <gui@gmail.com>
2026-09-16 19:45:25 -0300

    subindo

--- DoceCantinho.Infrastructure/Migrations/20260916223029_doce1.Designer.cs
+++ DoceCantinho.Infrastructure/Migrations/20260916223029_doce1.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoceCantinho.Infrastructure.Migrations
 {
     [DbContext(typeof(DoceCantinhoDbContext))]
-    [Migration("20260916201523_doce1")]
+    [Migration("20260916223029_doce1")]
     partial class doce1
     {
         /// <inheritdoc />
